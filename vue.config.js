ab78f9666a1d0b0f885766522dee4257f65c86c7
杨佳 <jia1.yang@changhong.com>
2019-06-12 11:45:39 +0800

    更改

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  publicPath: process.env.NODE_ENV === 'production' ? '/v-blog' : '/',
+  // publicPath: process.env.NODE_ENV === 'production' ? '/v-blog' : '/',
   outputDir: 'docs',
   assetsDir: './'
 }
